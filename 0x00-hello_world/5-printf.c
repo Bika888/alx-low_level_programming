ea81cac92ee1b4649b6d6825cbd002a162e8dbdb
Bika888 <bikaz1111@gmail.com>
2023-09-09 22:13:26 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - print"with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ * return: always 0
+ */
+
+int main(void)
+{
+  printf("ith proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
+}
